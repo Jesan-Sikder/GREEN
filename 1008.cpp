7e16740debadfdd28c09b54e8fd32fcb1bc870d1
Jesan Sikder <jesanalpha545@gmail.com>
2025-01-10 18:04:57 +0600

    .

--- 1008.cpp
+++ 1008.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  //upcoming................
+
+  float a,b,c;
+  cin>>a>>b>>c;
+cout<<"NUMBER = "<<(int)a<<endl;
+
+  cout<<fixed<<setprecision(2)<<"SALARY = U$ "<<b*c<<endl;
 }
